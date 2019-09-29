c938fe4d87b663e7e4181b7282514dc1d3329d13
Madhavan <lmadhavan@users.noreply.github.com>
2019-09-29 12:13:23 +0530

    Fill in BlendMode enum to match Win2D blend modes
    This completes step 9 of https://www.photoshoptutorials.ws/photoshop-tutorials/photo-effects/create-powerful-emotional-effect-portraits-9-easy-steps/

--- Fotografix.Editor/BlendMode.cs
+++ Fotografix.Editor/BlendMode.cs
@@ -3,7 +3,36 @@
     public enum BlendMode
     {
         Normal,
+        Dissolve,
+
+        Darken,
         Multiply,
-        Screen
+        ColorBurn,
+        LinearBurn,
+        DarkerColor,
+
+        Lighten,
+        Screen,
+        ColorDodge,
+        LinearDodge,
+        LighterColor,
+
+        Overlay,
+        SoftLight,
+        HardLight,
+        VividLight,
+        LinearLight,
+        PinLight,
+        HardMix,
+
+        Difference,
+        Exclusion,
+        Subtract,
+        Divide,
+
+        Hue,
+        Saturation,
+        Color,
+        Luminosity
     }
 }
