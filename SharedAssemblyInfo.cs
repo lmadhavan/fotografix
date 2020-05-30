2e32e5c0272793b1b0108d1b48267bbf5a9b98e1
Madhavan <lmadhavan@users.noreply.github.com>
2020-05-30 12:44:07 -0700

    Update version information for alpha release

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Fotografix")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyFileVersion("2.2006.1.0")]
