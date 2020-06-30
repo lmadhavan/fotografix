f1c03a4638b4d5b09160ef6a1c02aeec89b9f79b
Madhavan <lmadhavan@users.noreply.github.com>
2020-06-29 20:35:09 -0700

    Display error dialog for unhandled exceptions
    Closes #23

--- Fotografix/App.xaml.cs
+++ Fotografix/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Fotografix.UI;
+using System;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 
@@ -9,6 +11,8 @@ namespace Fotografix
         public App()
         {
             this.InitializeComponent();
+            this.UnhandledException += OnUnhandledException;
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
         }
 
         protected override void OnLaunched(LaunchActivatedEventArgs e)
@@ -27,6 +31,23 @@ namespace Fotografix
                 container.OpenStartPage();
                 Window.Current.Activate();
             }
+        }
+
+        private async void OnUnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            await ReportExceptionAsync(e.Exception);
+        }
+
+        private async void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            e.SetObserved();
+            await ReportExceptionAsync(e.Exception);
+        }
+
+        private async Task ReportExceptionAsync(Exception ex)
+        {
+            await new ErrorDialog(ex).ShowAsync();
         }
     }
 }
