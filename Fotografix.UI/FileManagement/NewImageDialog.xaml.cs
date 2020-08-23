6ebed3fb5169eefc78028e8e879e63a2333ac66e
Madhavan <lmadhavan@users.noreply.github.com>
2020-08-23 12:53:11 -0700

    Extract shared NumberBox style for dimension inputs

--- Fotografix.UI/FileManagement/NewImageDialog.xaml.cs
+++ Fotografix.UI/FileManagement/NewImageDialog.xaml.cs
@@ -1,17 +1,14 @@
-﻿using Windows.Globalization.NumberFormatting;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace Fotografix.UI.FileManagement
 {
     public sealed partial class NewImageDialog : ContentDialog
     {
         private readonly NewImageParameters parameters;
-        private readonly INumberFormatter2 numberFormatter;
 
         public NewImageDialog(NewImageParameters parameters)
         {
             this.parameters = parameters;
-            this.numberFormatter = NumberFormatters.Dimension;
             this.InitializeComponent();
         }
     }
--- Fotografix.UI/ResizeImageDialog.xaml.cs
+++ Fotografix.UI/ResizeImageDialog.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Fotografix.Editor;
-using Windows.Globalization.NumberFormatting;
 using Windows.UI.Xaml.Controls;
 
 namespace Fotografix.UI
@@ -7,12 +6,10 @@ namespace Fotografix.UI
     public sealed partial class ResizeImageDialog : ContentDialog
     {
         private readonly ResizeImageParameters parameters;
-        private readonly INumberFormatter2 numberFormatter;
 
         public ResizeImageDialog(ResizeImageParameters parameters)
         {
             this.parameters = parameters;
-            this.numberFormatter = NumberFormatters.Dimension;
             this.InitializeComponent();
         }
     }
