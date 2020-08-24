5d27f77f4413e59be545762a17ba71b299d78324
Madhavan <lmadhavan@users.noreply.github.com>
2020-08-23 18:28:34 -0700

    Display tool settings in UI
    Closes #40

---
+++ Fotografix.UI/Tools/ToolTemplatesResourceDictionary.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Fotografix.Editor.Tools;
+
+namespace Fotografix.UI.Tools
+{
+    public partial class ToolTemplatesResourceDictionary
+    {
+        public ToolTemplatesResourceDictionary()
+        {
+            InitializeComponent();
+
+            toolSettingsTemplateSelector.SetTemplate<BrushTool>(brushToolSettingsTemplate);
+            toolSettingsTemplateSelector.SetTemplate<GradientTool>(gradientToolSettingsTemplate);
+        }
+    }
+}
--- Fotografix.UI/TypeBasedTemplateSelector.cs
+++ Fotografix.UI/TypeBasedTemplateSelector.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-namespace Fotografix.UI.Adjustments
+namespace Fotografix.UI
 {
-    public sealed class AdjustmentTemplateSelector : DataTemplateSelector
+    public sealed class TypeBasedTemplateSelector : DataTemplateSelector
     {
         private readonly Dictionary<Type, DataTemplate> templates = new Dictionary<Type, DataTemplate>();
 
