03b3728e0f073e606150a911c519d1461d1e9a6d
Madhavan <lmadhavan@users.noreply.github.com>
2020-08-23 18:10:08 -0700

    Extract ColorPickerButton into a user control so it can be reused

---
+++ Fotografix.UI/ColorPickerButton.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Fotografix.UI
+{
+    public sealed partial class ColorPickerButton : UserControl
+    {
+        public ColorPickerButton()
+        {
+            InitializeComponent();
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return (Color)GetValue(ColorProperty);
+            }
+
+            set
+            {
+                if (Color != value)
+                {
+                    SetValue(ColorProperty, value);
+                }
+            }
+        }
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(nameof(Color), typeof(int), typeof(ColorPickerButton), new PropertyMetadata(Colors.Black));
+    }
+}
