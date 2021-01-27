e7620caf5192885bf95e6390877c0c421def8af7
Madhavan <lmadhavan@users.noreply.github.com>
2021-01-26 17:53:57 -0800

    Render resize handles for crop preview
    For #2

--- Fotografix.Win2D/Composition/CropPreviewNode.cs
+++ Fotografix.Win2D/Composition/CropPreviewNode.cs
@@ -1,20 +1,37 @@
 ﻿using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Geometry;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Numerics;
-using Windows.Foundation;
-using Windows.UI;
 
 namespace Fotografix.Win2D.Composition
 {
     internal sealed class CropPreviewNode : IDisposable
     {
-        private static readonly Color DimColor = Color.FromArgb(128, 128, 128, 128);
+        private const int HandleSize = 8;
+
+        private static readonly Windows.UI.Color DimColor = Windows.UI.Color.FromArgb(128, 128, 128, 128);
+        private static readonly Windows.UI.Color OuterStrokeColor = Windows.UI.Colors.Black;
+        private static readonly Windows.UI.Color InnerStrokeColor = Windows.UI.Colors.White;
+        private static readonly Windows.UI.Color FillColor = Windows.UI.Colors.White;
+
+        private static readonly List<PointF> HandleLocations = new List<PointF>
+        {
+            new PointF(0.0f, 0.0f),
+            new PointF(0.5f, 0.0f),
+            new PointF(1.0f, 0.0f),
+            new PointF(0.0f, 0.5f),
+            new PointF(1.0f, 0.5f),
+            new PointF(0.0f, 1.0f),
+            new PointF(0.5f, 1.0f),
+            new PointF(1.0f, 1.0f)
+        };
 
         private readonly CanvasGeometry dimmedArea;
-        private readonly Rect cropRect;
+        private readonly Rectangle cropRect;
 
-        public CropPreviewNode(ICanvasResourceCreator resourceCreator, System.Drawing.Size imageSize, System.Drawing.Rectangle cropRectangle)
+        public CropPreviewNode(ICanvasResourceCreator resourceCreator, Size imageSize, Rectangle cropRectangle)
         {
             using (CanvasGeometry imageBoundsGeometry = CanvasGeometry.CreateRectangle(resourceCreator, 0, 0, imageSize.Width, imageSize.Height))
             using (CanvasGeometry cropRectangleGeometry = CanvasGeometry.CreateRectangle(resourceCreator, cropRectangle.X, cropRectangle.Y, cropRectangle.Width, cropRectangle.Height))
@@ -22,7 +39,7 @@ namespace Fotografix.Win2D.Composition
                 this.dimmedArea = imageBoundsGeometry.CombineWith(cropRectangleGeometry, Matrix3x2.Identity, CanvasGeometryCombine.Exclude);
             }
 
-            this.cropRect = cropRectangle.ToWindowsRect();
+            this.cropRect = cropRectangle;
         }
 
         public void Dispose()
@@ -32,8 +49,47 @@ namespace Fotografix.Win2D.Composition
 
         public void Draw(CanvasDrawingSession ds)
         {
+            void OuterStroke(RectangleF r)
+            {
+                ds.DrawRectangle(r.X - 0.5f, r.Y - 0.5f, r.Width + 1f, r.Height + 1f, OuterStrokeColor);
+            }
+
+            void InnerStroke(RectangleF r)
+            {
+                ds.DrawRectangle(r.X + 0.5f, r.Y + 0.5f, r.Width - 1f, r.Height - 1f, InnerStrokeColor);
+            }
+
+            void Fill(RectangleF r)
+            {
+                ds.FillRectangle(r.X, r.Y, r.Width, r.Height, FillColor);
+            }
+
             ds.FillGeometry(dimmedArea, DimColor);
-            ds.DrawRectangle(cropRect, Colors.White);
+
+            OuterStroke(cropRect);
+            InnerStroke(cropRect);
+
+            foreach (PointF hl in HandleLocations)
+            {
+                RectangleF r = GetHandleRectangle(hl);
+                OuterStroke(r);
+                Fill(r);
+            }
+        }
+
+        private RectangleF GetHandleRectangle(PointF hl)
+        {
+            PointF pt = new PointF(
+                cropRect.X + hl.X * cropRect.Width,
+                cropRect.Y + hl.Y * cropRect.Height
+            );
+
+            return RectangleF.FromLTRB(
+                pt.X - HandleSize / 2,
+                pt.Y - HandleSize / 2,
+                pt.X + HandleSize / 2,
+                pt.Y + HandleSize / 2
+            );
         }
     }
 }
